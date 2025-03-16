245d948e2712619d6bfb2d046a7685b8b672a7b3
ZXX <zhangxixiang2021@outlook.com>
2025-03-17 00:44:03 +0800

    还有一个没删完。。。

--- blog-frontend/src/views/Create.vue
+++ blog-frontend/src/views/Create.vue
@@ -51,32 +51,3 @@ import Tiptap from '../components/Tiptap.vue'
   padding-top: 60px;
 }
 </style> -->
-
-
-<div class="container">
-  </div>
-<Editor @content-change="handleContentChange" />
-<div v-if="editorContent">
-    <div v-html="editorContent"></div>
-</div>
-</template>
-
-<script setup>
-import { ref } from 'vue';
-import Editor from '../components/Editor.vue'
-
-const editorContent = ref(''); // 用于存储从子组件传递过来的内容
-
-// 处理子组件传递过来的内容
-const handleContentChange = (content) => {
-  editorContent.value = content;
-  console.log('接收到编辑器内容：', content);
-};
-</script>
-
-<style lang="scss" scoped>
-.container {
-  margin: 0 20%;
-  padding-top: 60px;
-}
-</style> -->
